e63208af0e90ba39f4d0c744940634da00c8589e
sahill01 <sahilravipandey786@gmail.com>
2023-07-20 07:09:55 +0530

    Updated project files

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer id="contact" className="contact">
+      <p>Contact Me  :  <a href="mailto:sahilravipandey786@gmail.com">sahilravipandey786@gmail.com</a></p>
+      <p>Author of this Blog: Sahil Pandey</p>
+      <p><p>&copy; 2023 by Sahil. All rights reserved.</p></p>
+    </footer>
+  );
+};
+
+export default Footer;
